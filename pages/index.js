fe21a45592a54c19954988cc8efeb2c1bfdcffab
ahmadrosid <alahmadrosid@gmail.com>
2021-10-27 22:51:33 +0700

    feat: add initial editable content

--- pages/index.js
+++ pages/index.js
@@ -2,81 +2,36 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="bg-gray-200 p-8">
       <Head>
-        <title>Create Next App</title>
+        <title>Resumine - Resume maker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="w-full max-w-[900px] mx-auto bg-white flex justify-between">
+        <div className="w-[400px] space-y-8 p-8">
+          <div className="w-52 h-52 bg-gray-400 rounded-full"></div>
+          <div className="space-y-4">
+            <div className="">
+              <h2 className="font-semibold text-gray-700 text-2xl uppercase p-2 focus:outline-none focus:bg-gray-300 focus:rounded" contentEditable="true">Profile</h2>
+              <p className="text-gray-500 p-2 focus:outline-none focus:bg-gray-300 focus:rounded" contentEditable="true">Write here a paragraph about yourself.</p>
+            </div>
+            <div className="">
+              <h2 className="font-semibold text-gray-700 text-2xl uppercase p-2 focus:outline-none focus:bg-gray-300 focus:rounded" contentEditable="true">Personal detail</h2>
+              <div>
+                <h3 className="text-green-500 font-semibold text-xl p-2 focus:outline-none focus:bg-gray-300 focus:rounded" contentEditable="true">Address</h3>
+                <p className="text-gray-500 p-2 focus:outline-none focus:bg-gray-300 focus:rounded" contentEditable="true">Enter your email address</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <div className="w-full">
+          <div className="p-8 pt-[80px]">
+            <h1 className="text-5xl text-gray-700 font-bold p-2 focus:outline-none focus:bg-gray-300 focus:rounded" contentEditable="true">Your name</h1>
+            <h2 className="text-2xl font-semibold text-gray-500 uppercase p-2 focus:outline-none focus:bg-gray-300 focus:rounded" contentEditable="true">Your profession or speciality</h2>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
